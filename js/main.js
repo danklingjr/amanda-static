2bc6736da436f6b5c1d33ec0a144a19fae48ce97
Daniel Kling <dan@717studios.com>
2015-07-17 20:13:58 -0400

    preparing wordpress

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+$('.instagram').instagram({
+  userId: 252729846,
+  accessToken: '2942968.f0a11ef.ce41f12eeac14b57ac79e84ec72a70ca',
+  show : 9
+});
---
+++ js/min/main-min.js
@@ -0,0 +1,1 @@
+$(".instagram").instagram({userId:252729846,accessToken:"2942968.f0a11ef.ce41f12eeac14b57ac79e84ec72a70ca",show:9});
